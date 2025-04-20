e9ec78f53a389a5f18b8b9fcb061f76a2ae3b6b0
orangci <orangc@proton.me>
2025-04-20 12:54:29 +0300

    meta: add spdx identifier for copyright holder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-FileCopyrightText: orangc
 from datetime import datetime, timedelta
 import subprocess, os, argparse, time, json, urllib.request
 
